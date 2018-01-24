820d6b1715e4ff12878a02b21f117e720f664668
ylkjcjq <691424873@qq.com>
2018-01-24 22:18:20 +0800

    increment

--- api/models/box.mining.model.js
+++ api/models/box.mining.model.js
@@ -4,18 +4,20 @@ const sequelize = require('../domain/promoserver.prepare').sequelize;
 const TABLE_DEFINE = require("../domain/table.define");
 const DomainCoinEveryDay = TABLE_DEFINE.DomainCoinEveryDay;
 const DomainAccountBox = TABLE_DEFINE.DomainAccountBox;
+const DomainAccountBoxConnect = TABLE_DEFINE.DomainAccountBoxConnect;
 const DomainBoxStatus = TABLE_DEFINE.DomainBoxStatus;
 const moment = require('moment');
 var ModelBoxMining = module.exports;
 
 //每刻 挖币 产币
 ModelBoxMining.addBoxMining = function addBoxMining(body) {
-    return DomainAccountBox.findOne({
+    return DomainAccountBoxConnect.findOne({
         where:{
             boxSN:body.boxSN,
             isBinding:true
         }
     }).then((boxaccount)=>{
+        console.log(JSON.stringify(boxaccount));
         if(boxaccount == null || boxaccount == undefined){
             return {
                 isSuccess:false,
@@ -24,13 +26,11 @@ ModelBoxMining.addBoxMining = function addBoxMining(body) {
         }else{
             let today = moment(new Date()).format('YYYY-MM-DD');
             return DomainCoinEveryDay.create({
-                where:{
                     account: boxaccount.account,
                     boxSN: body.boxSN,
                     boxIp:body.boxIp,
-                    miningCoin:body.miningCoin,
+                    miningCoin:body.miningCoin == null ? 0.0 : body.miningCoin,
                     today:today
-                }
             }).then((data)=>{
                 if(data == null || data == undefined){
                     return {
@@ -38,19 +38,17 @@ ModelBoxMining.addBoxMining = function addBoxMining(body) {
                         reason:"插入失败"
                     };
                 }else{
-                    return DomainAccountBox.update(
-                        {
-                        totalMiningCoin:boxaccount.totalMiningCoin + body.miningCoin,
-                        remainMiningCoin:boxaccount.remainMiningCoin + body.miningCoin
-                        },{
+                    return DomainAccountBox.findOne({
                         where:{
                             account:boxaccount.account
                         }
                     }).then((date)=>{
-                        return {    
-                            isSuccess:true,
-                            reason:"保存成功"
-                        };
+                        return date.increment({totalMiningCoin:data.miningCoin, remainMiningCoin:data.miningCoin}).then(function(user){
+                            return {    
+                                isSuccess:true,
+                                reason:"保存成功"
+                            };
+                        })
                     });
                 }
             });
